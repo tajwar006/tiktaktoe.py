79f96d90a613fdcb6f6e39dbf3c352c074ef068b
18mahmed1 <150432903+18mahmed1@users.noreply.github.com>
2023-11-10 10:46:13 +0000

    Add files via upload

---
+++ Tictactoe.py
@@ -0,0 +1,20 @@
+  # Check rows
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    # Check columns
+    for col in range(3):
+        if all(row[col] == player for row in board):
+            return True
+
+    # Check diagonals
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+     # Check for a winner
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            
+           
