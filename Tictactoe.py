654003184492d25d7f6f43f34c29a5e17aa3bbe9
18mahmed1 <150432903+18mahmed1@users.noreply.github.com>
2023-11-16 11:24:54 +0000

    Add files via upload

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,20 +1,38 @@
-  # Check rows
-    for row in board:
-        if all(cell == player for cell in row):
-            return True
+def main():
+    # Initialize the board
+    board = [[' ' for _ in range(3)] for _ in range(3)]
 
-    # Check columns
-    for col in range(3):
-        if all(row[col] == player for row in board):
-            return True
+    # Variable to keep track of the current player ('X' or 'O')
+    current_player = 'X'
 
-    # Check diagonals
-    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
-        return True
+    while True:
+        print_board(board)
 
-     # Check for a winner
+        # Get the user input for the move
+        row = int(input(f"Enter the row (0, 1, 2) for {current_player}: "))
+        col = int(input(f"Enter the column (0, 1, 2) for {current_player}: "))
+
+        # Check if the cell is empty
+        if board[row][col] == ' ':
+            board[row][col] = current_player
+        else:
+            print("Cell is already occupied. Try again.")
+            continue
+
+        # Check for a winner
         if check_winner(board, current_player):
             print_board(board)
             print(f"Player {current_player} wins!")
-            
-           
+            break
+
+        # Check for a draw
+        if check_draw(board):
+            print_board(board)
+            print("The game ends in a draw!")
+            break
+
+        # Switch to the other player
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    main()
