9a592c6f138e70f07d3267c7e745eecaee91daba
tajwar006 <150432610+tajwar006@users.noreply.github.com>
2023-11-20 10:55:15 +0000

    Update tiktaktoe.py

--- tiktaktoe.py
+++ tiktaktoe.py
@@ -1,16 +1,16 @@
-def print_board(board):
+def printBoard(board):
     for row in board:
         print("|".join(row))
         print("-" * 5)
 
-def check_draw(board):
+def Drawn(board):
     for row in board:
         for cell in row:
             if cell == ' ':
                 return False
     return True
 
-def check_winner(board, player):
+def Winner(board, player):
     for row in board:
         if all(cell == player for cell in row):
             return True
@@ -28,7 +28,7 @@ def main():
     board = [[' ' for _ in range(3)] for _ in range(3)]
 
     while True:
-        print_board(board)
+        printBoard(board)
 
         row = int(input(f"Enter the row (0, 1, 2) for {current_player}: "))
         col = int(input(f"Enter the column (0, 1, 2) for {current_player}: "))
@@ -36,17 +36,17 @@ def main():
         if board[row][col] == ' ':
             board[row][col] = current_player
         else:
-            print("Cell is already occupied. Try again.")
+            print("This place is taken. Try again.")
             continue
 
-        if check_winner(board, current_player):
-            print_board(board)
+        if Winner(board, current_player):
+            PrintBoard(board)
             print(f"Player {current_player} wins!")
             break
 
-        if check_draw(board):
-            print_board(board)
-            print("The game ends in a draw!")
+        if Drawn(board):
+            PrintBoard(board)
+            print("The game is a draw!")
             break
         current_player = 'O' if current_player == 'X' else 'X'
 
